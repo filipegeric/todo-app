5dabc51bacf1cdfd64f5b945f622a1a20af4153c
filipegeric <filip.egeric@gmail.com>
2024-07-27 09:33:42 +0200

    Add rest controller

---
+++ todo-server/src/main/kotlin/org/example/todo/todos/TodosController.kt
@@ -0,0 +1,31 @@
+package org.example.todo.todos
+
+import org.example.todo.auth.User
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/todos")
+class TodosController(private val service: TodosService) {
+
+    @PostMapping
+    fun create(
+        @RequestBody request: CreateTodoRequest,
+        user: User,
+    ) = service.createTodo(request, user)
+
+    @GetMapping
+    fun getTodos(user: User) = service.findTodos(user)
+
+    @PatchMapping("/{id}")
+    fun updateTodo(
+        @PathVariable id: Long,
+        @RequestBody request: UpdateTodoRequest,
+        user: User,
+    ) = service.updateTodo(request, user)
+}
--- todo-server/src/main/kotlin/org/example/todo/todos/TodosModule.kt
+++ todo-server/src/main/kotlin/org/example/todo/todos/TodosModule.kt
@@ -1,7 +1,11 @@
 package org.example.todo.todos
 
 import org.example.todo.id.UUIDGenerator
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 class TodosModule {
+    @Bean
     fun todosService(): TodosService = TodosService(InMemoryTodosRepository(), UUIDGenerator)
 }
